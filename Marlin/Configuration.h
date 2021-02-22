8e708044f30570b700d70c437cf9d94c04fadf6a
Till Fischer <github@fischer-siegen.de>
2021-02-22 23:43:51 +0100

    Back to Z210 STEPS_PER_UNIT

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -742,7 +742,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 200 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 160, 160, 800, 210 }
 
 /**
  * Default Max Feed Rate (mm/s)
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -20, -27, -1.4 }
+#define NOZZLE_TO_PROBE_OFFSET { -20, -27, -1.2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
