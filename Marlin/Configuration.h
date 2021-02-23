6592ed7b20b266607303a2a5fa50464cc4050698
Till Fischer <github@fischer-siegen.de>
2021-02-23 11:17:54 +0100

    NOZZLE_TO_PROBE_OFFSET 0 //for UBL

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -987,7 +987,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -20, -27, -1.2 }
+#define NOZZLE_TO_PROBE_OFFSET { -20, -27, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
